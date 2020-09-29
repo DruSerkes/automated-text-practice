1434b9de6205b8585242b63359583774a7d368f9
Dru Serkes <DruSerkes@gmail.com>
2020-09-29 01:09:33 -0400

    add good-morning script

---
+++ good-morning.py
@@ -0,0 +1,24 @@
+import schedule
+from twilio.rest import Client
+from secrets import auth_token, account_sid, twilio_number, daily_tag, weinberg_number, good_morning_messages
+import time
+from random import choice
+
+
+def job():
+    client = Client(account_sid, auth_token)
+    message = choice(good_morning_messages)
+    print(f"Message sent: {message}")
+    client.messages.create(
+        to=weinberg_number, from_=twilio_number, body=message)
+    schedule.clear(daily_tag)
+
+
+schedule.every().day.at("9:00").do(job).tag(daily_tag)
+
+while True:
+    schedule.run_pending()
+    time.sleep(5)
+    if not schedule.jobs:
+        print("No more jobs - ending process")
+        break
